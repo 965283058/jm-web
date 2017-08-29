d9ea13666aac6a30bf468220c875037f1ed76dd5
jiangpeng <jiangpeng@houbank.com>
2017-08-29 22:13:14 +0800

    修改端口

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(async(ctx, next)=> {
 })
 
 app.use(async(ctx, next)=> {
-    ctx.serverOrigin = 'http://127.0.0.1:9999'
+    ctx.serverOrigin = 'http://47.93.29.253'
     await next()
 })
 
